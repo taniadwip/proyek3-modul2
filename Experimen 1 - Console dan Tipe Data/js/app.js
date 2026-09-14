6e5c382df74f43a8cf376499f09c20931b6a7174
taniadwip <taniadwip15@gmail.com>
2026-09-14 10:46:43 +0700

    Eksperimen 1

---
+++ Experimen 1 - Console dan Tipe Data/js/app.js
@@ -0,0 +1,14 @@
+const nama = "Rani";
+const nilaiTeks = "80";
+const nilaiAngka = 80;
+const aktif = true;
+
+console.log(typeof nama);
+console.log(typeof nilaiTeks);
+console.log(typeof nilaiAngka);
+console.log(typeof aktif);
+console.log(nilaiTeks + 5);
+console.log(Number(nilaiTeks) + 5);
+console.log(nilaiTeks == nilaiAngka);
+console.log(nilaiTeks === nilaiAngka);
+console.log(Number(nama));
